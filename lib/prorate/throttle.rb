d433853f757d9acb3a6b86bbf6073ac7e0a439ea
Wander Hillen <wander@wetransfer.com>
2017-07-13 17:40:06 +0200

    Add fallback for when script is not known yet to redis

--- lib/prorate/throttle.rb
+++ lib/prorate/throttle.rb
@@ -1,6 +1,8 @@
 require 'digest'
 
 module Prorate
+
+
   class Throttle < Ks.strict(:name, :limit, :period, :block_for, :redis, :logger)
     def initialize(*)
       super
@@ -17,24 +19,30 @@ module Prorate
       identifier = [name, discriminator].join(':')
       
       redis.with do |r|
-        #logger.info { "Checking throttle block %s" % name }
-        #raise Throttled.new(block_for) if Prorate::BlockFor.blocked?(id: identifier, redis: r)
-
         logger.info { "Applying throttle counter %s" % name }
-        # allowed request rate is limit per period
         allowed_req_rate = period / limit
         bucket_capacity = limit # how many tokens can be in the bucket
         leak_rate = period / limit # tokens per second; 
-        weight = 1 # how many tokens each request is worth
-        resp = r.evalsha('ac3eeb0e21c24a41442e44627ece11f182a1d382', [], [identifier, bucket_capacity, leak_rate, weight, block_for])
+        # weight = 1 # how many tokens each request is worth
+        resp = run_lua_throttler(redis: r, identifier: identifier, bucket_capacity: bucket_capacity, leak_rate: leak_rate, weight: 1, block_for)
 
-        #c = Prorate::Counter.new(redis: r, id: identifier, logger: logger, window_size: period)
-        #after_increment = c.incr
         if resp != "OK"
           logger.warn { "Throttle %s exceeded limit of %d at %d" % [name, limit, after_increment] }
-          #Prorate::BlockFor.block!(redis: r, id: identifier, duration: block_for)
-          raise Throttled.new(resp)
+          raise Throttled.new("Throttled, please lower your temper and try again in #{resp} seconds")
         end
+      end
+    end
+
+    def run_lua_throttler(redis: , identifier: , bucket_capacity: , leak_rate: , weight: , block_for)
+      # Slightly magic hash:
+      redis.evalsha('ac3eeb0e21c24a41442e44627ece11f182a1d382', [], [identifier, bucket_capacity, leak_rate, weight, block_for])
+    rescue Redis::CommandError => e
+      if e.message.include? "NOSCRIPT"
+        # The Redis server has never seen this script before. Needs to run only once in the entire lifetime of the server (unless the script changes)
+        script_filepath = File.join(__dir__,"rate_limit.lua")
+        script = File.read(script_filepath)
+        redis.script(:load, script)
+        redis.evalsha('ac3eeb0e21c24a41442e44627ece11f182a1d382', [], [identifier, bucket_capacity, leak_rate, weight, block_for])
       end
     end
   end
