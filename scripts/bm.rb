2f4c5abb246cd9d373bf7cf363ae1f8cfccde9ad
Wander Hillen <wander@wetransfer.com>
2017-07-18 11:21:36 +0200

    Update iterations in test script
    As it turned out that there was a effect where the first few
    iterations were slower and were pulling up the average.

--- scripts/bm.rb
+++ scripts/bm.rb
@@ -15,9 +15,9 @@ logz = Logger.new(STDERR)
 logz.level = Logger::FATAL # block out most stuff
 
 times = []
-15.times do
+50.times do
   times << Benchmark.realtime {
-    t = Prorate::Throttle.new(redis: r, logger: logz, name: "throttle-login-email", limit: 60, period: 120, block_for: 5)
+    t = Prorate::Throttle.new(redis: r, logger: logz, name: "throttle-login-email", limit: 60, period: 30, block_for: 5)
     # Add all the parameters that function as a discriminator
     t << '127.0.2.1'
     t << 'no_person@nowhere.com'
@@ -28,11 +28,11 @@ end
 puts average_ms times
 
 times = []
-15.times do
+50.times do
   email = SecureRandom.hex(20)
   ip = SecureRandom.hex(10)
   times << Benchmark.realtime {
-    t = Prorate::Throttle.new(redis: r, logger: logz, name: "throttle-login-email", limit: 20, period: 120, block_for: 5)
+    t = Prorate::Throttle.new(redis: r, logger: logz, name: "throttle-login-email", limit: 30, period: 30, block_for: 5)
     # Add all the parameters that function as a discriminator
     t << ip
     t << email
